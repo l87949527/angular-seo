b51449666c3bf015e6df430fa2a445d47b8ad8e4
l87949527 <l87949527@qq.com>
2013-12-18 17:43:35 +0800

    test directive

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,8 +3,9 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
+  controller('MyCtrl1', ['$scope',function($scope) {
+        $scope.titile = "Click me to expand";
+        $scope.text = "Hi there folks, I am the content that was hidden but is now shown.";
   }])
   .controller('MyCtrl2', [function() {
 
--- app/js/directives.js
+++ app/js/directives.js
@@ -3,9 +3,39 @@
 /* Directives */
 
 
-angular.module('myApp.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
+var appDirective = angular.module('myApp.directives', []);
+
+
+appDirective.directive('appVersion', ['version', function (version) {
+    return function (scope, elm, attrs) {
+        elm.text(version);
     };
-  }]);
+}]);
+
+
+appDirective.directive('aGreatEye', function () {
+    return {
+        transclude: true,
+        template: '<h1>God! You are <span ng-transclude></span> times</h1>'
+    };
+});
+
+
+appDirective.directive('myComment', function () {
+    return {
+        restrict: 'AE',
+        replace:  false,
+        templateUrl: '/app/partials/template.html'
+    };
+});
+
+appDirective.directive('expander',[function(){
+    return {
+        restrict: 'EA',
+        replace: true,
+        transclude: true,
+        scope: {title: '=expanderTitle'},
+        template: '<div>' +
+            '<div class="title" ng-click="toggle()">'
+    }
+}]);
