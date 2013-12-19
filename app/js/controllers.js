2d4669d23e2eb1a0a65c0abf6e01a87b2c34193b
l87949527 <l87949527@qq.com>
2013-12-19 17:01:56 +0800

    add directive

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,10 +3,28 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', ['$scope',function($scope) {
+    controller('MyCtrl1', ['$scope', function ($scope) {
+        $scope.expanders = [
+            {
+                title: "click one",
+                text: "text one \n test one"
+            },
+            {
+                title: 'click two',
+                text: 'text two \n test two'
+            },
+            {
+                title: 'click three',
+                text: 'text three \n three three'
+            }
+        ]
         $scope.title = "Click me to expand";
         $scope.text = "Hi there folks, I am the content that was hidden but is now shown.";
-  }])
-  .controller('MyCtrl2', [function() {
 
-  }]);
+        $scope.callMe = function callMe(message) {
+            alert(message);
+        }
+    }])
+    .controller('MyCtrl2', [function () {
+
+    }]);
--- app/js/directives.js
+++ app/js/directives.js
@@ -29,21 +29,56 @@ appDirective.directive('myComment', function () {
     };
 });
 
+appDirective.directive('accordion',[function(){
+    return {
+        restrict: 'EA',
+        replace: true,
+        transclude: true,
+        template: '<div ng-transclude></div>',
+        controller: function(){
+            var expanders = [];
+            this.gotOpened = function(selectedExpender){
+                angular.forEach(expanders,function(expander){
+                    if(selectedExpender != expander){
+                        expander.showMe = false;
+                    }
+                });
+            };
+            this.addExpander = function(expander){
+                expanders.push(expander);
+            }
+        }
+    }
+}]);
+
 appDirective.directive('expander',[function(){
     return {
         restrict: 'EA',
         replace: true,
         transclude: true,
-        scope: {title: '=expanderTitle'},
+        controller: function(){
+            this.addExpander = function(){
+
+            }
+        },
+        require: '?^accordion',
+        scope: {
+            title: '=expanderTitle',
+            call : '&'
+        },
         template: '<div>' +
             '<div class="title" ng-click="toggle()">{{title}}</div>' +
             '<div class="body" ng-show="showMe" ng-transclude></div>'+
+            '<input ng-model="value"/>'+
+            '<button ng-click="call({message:value})">call me</button>' +
             '</div>',
-        link: function(scope, element, attrs){
+        link: function(scope, element, attrs, accordionController){
             scope.showMe = false;
+            accordionController.addExpander(scope);
 
-            scope.toggle = function toggle(){
+            scope.toggle = function toggle() {
                 scope.showMe = !scope.showMe;
+                accordionController.gotOpened(scope);
             }
         }
     }
